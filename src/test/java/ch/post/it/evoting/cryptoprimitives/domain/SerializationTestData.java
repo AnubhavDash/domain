6644af7ceffe40dcc61487c933e20e509651e15e
jenkinsevoting <spoc-dev-evoting@post.ch>
2021-11-26 10:00:20 +0100

    merge release crypto-primitives-domain 0.12.0.4

--- src/test/java/ch/post/it/evoting/cryptoprimitives/domain/SerializationTestData.java
+++ src/test/java/ch/post/it/evoting/cryptoprimitives/domain/SerializationTestData.java
@@ -296,7 +296,6 @@ public class SerializationTestData extends MapperSetUp {
 		return rootNode;
 	}
 
-
 	// ===============================================================================================================================================
 	// Arguments creation.
 	// ===============================================================================================================================================
@@ -408,7 +407,7 @@ public class SerializationTestData extends MapperSetUp {
 	}
 
 	public static ReturnCodeGenerationResponsePayload getResponsePayload(final String tenantId, final String electionEventId,
-																		 final String verificationCardSetId, final int chunkId) {
+			final String verificationCardSetId, final int chunkId) {
 
 		final List<ReturnCodeGenerationOutput> returnCodeGenerationOutputs = Arrays
 				.asList(getReturnCodeGenerationOutput("1"), getReturnCodeGenerationOutput("2"));
@@ -462,7 +461,9 @@ public class SerializationTestData extends MapperSetUp {
 	}
 
 	public static ReturnCodeGenerationRequestPayload getRequestPayload(final Ballot ballot, final String tenantId, final String electionEventId,
-																	   final String verificationCardSetId, final int chunkId) {
+			final String verificationCardSetId, final int chunkId) {
+
+		final List<String> partialChoiceReturnCodesAllowList = Arrays.asList("a", "b", "c", "d");
 
 		final List<ElGamalMultiRecipientCiphertext> ciphertexts = getCiphertexts(2);
 		final ElGamalMultiRecipientPublicKey verificationCardPublicKey = getPublicKey();
@@ -473,7 +474,8 @@ public class SerializationTestData extends MapperSetUp {
 		final CombinedCorrectnessInformation combinedCorrectnessInformation = new CombinedCorrectnessInformation(ballot);
 
 		final ReturnCodeGenerationRequestPayload requestPayload = new ReturnCodeGenerationRequestPayload(tenantId, electionEventId,
-				verificationCardSetId, chunkId, gqGroup, returnCodeGenerationInputs, combinedCorrectnessInformation);
+				verificationCardSetId, partialChoiceReturnCodesAllowList, chunkId, gqGroup, returnCodeGenerationInputs,
+				combinedCorrectnessInformation);
 
 		// Generate random bytes for signature content and create payload signature.
 		final byte[] randomBytes = new byte[10];
@@ -490,6 +492,11 @@ public class SerializationTestData extends MapperSetUp {
 		rootNode.put("tenantId", requestPayload.getTenantId());
 		rootNode.put("electionEventId", requestPayload.getElectionEventId());
 		rootNode.put("verificationCardSetId", requestPayload.getVerificationCardSetId());
+
+		final JsonNode partialChoiceReturnCodesAllowListNode = mapper.readTree(
+				mapper.writeValueAsString(requestPayload.getPartialChoiceReturnCodesAllowList()));
+		rootNode.set("partialChoiceReturnCodesAllowList", partialChoiceReturnCodesAllowListNode);
+
 		rootNode.put("chunkId", requestPayload.getChunkId());
 
 		final JsonNode encryptionGroupNode = SerializationTestData.createEncryptionGroupNode(gqGroup);
