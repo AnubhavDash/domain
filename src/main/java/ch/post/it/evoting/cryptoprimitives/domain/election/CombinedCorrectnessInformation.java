70000254825a7d23d0fa8db25cff63e9663101bc
jenkinsevoting <spoc-dev-evoting@post.ch>
2021-12-14 10:59:37 +0100

    merge release crypto-primitives-domain 0.12.0.6

--- src/main/java/ch/post/it/evoting/cryptoprimitives/domain/election/CombinedCorrectnessInformation.java
+++ src/main/java/ch/post/it/evoting/cryptoprimitives/domain/election/CombinedCorrectnessInformation.java
@@ -40,6 +40,8 @@ import ch.post.it.evoting.cryptoprimitives.hashing.HashableList;
 public class CombinedCorrectnessInformation implements HashableList {
 
 	private static final String LISTS_AND_CANDIDATES_TEMPLATE = "listsAndCandidates";
+	private static final String OPTIONS_TEMPLATE = "options";
+
 	private static final String CANDIDATES = "candidates";
 	private static final int MAX_LISTS_AND_CANDIDATES_QUESTIONS_SIZE = 2;
 
@@ -75,7 +77,7 @@ public class CombinedCorrectnessInformation implements HashableList {
 	}
 
 	/**
-	 * Get the correctness id corresponding to the provided selection index.
+	 * Gets the correctness id corresponding to the provided selection index.
 	 *
 	 * @param index the selection index to get the corresponding correctness id. Must respect 0 <= index < number of voting options a voter can
 	 *              select.
@@ -93,7 +95,7 @@ public class CombinedCorrectnessInformation implements HashableList {
 	}
 
 	/**
-	 * Get the correctness id corresponding to the provided voting option index.
+	 * Gets the correctness id corresponding to the provided voting option index.
 	 *
 	 * @param index the voting option index to get the corresponding correctness id. Must respect 0 <= index < number of possible voting options.
 	 * @return the corresponding correctness id.
@@ -130,39 +132,133 @@ public class CombinedCorrectnessInformation implements HashableList {
 	}
 
 	private static List<CorrectnessInformation> getCorrectnessInformationListFromBallot(final Ballot ballot) {
-		checkContestsNotNullAndNotEmpty(ballot.getContests(), ballot.getId());
+		final List<Contest> contests = checkContestsNotNullAndNotEmpty(ballot.getContests(), ballot.getId());
 
 		final List<CorrectnessInformation> correctnessInformationList = new ArrayList<>();
 
-		for (final Contest contest : ballot.getContests()) {
+		for (final Contest contest : contests) {
 
 			final String contestId = contest.getId();
 			final String template = contest.getTemplate();
 			final List<Question> questions = contest.getQuestions();
 			final List<ElectionAttributes> attributes = contest.getAttributes();
+			final List<ElectionOption> options = contest.getOptions();
 
 			checkNotNullAndNotEmpty(questions, "questions", contestId);
-			checkQuestionsSizeOfContest(contestId, template, questions);
-
 			checkNotNullAndNotEmpty(attributes, "election attributes", contestId);
-			checkNotNullAndNotEmpty(contest.getOptions(), "election options", contestId);
+			checkNotNullAndNotEmpty(options, "election options", contestId);
 
-			if (isSwappingOfQuestionsNeeded(template, questions, attributes)) {
-				Collections.swap(questions, 0, 1);
-			}
-
-			for (final Question question : questions) {
-
-				final String correctnessId = question.getAttribute();
-				final Integer numberOfVotingOptions = getNumberOfVotingOptions(contest, correctnessId);
-				final Integer maximalSelections = question.getMax();
-
-				correctnessInformationList.add(new CorrectnessInformation(correctnessId, maximalSelections, numberOfVotingOptions));
-			}
+			correctnessInformationList.addAll(getCorrectnessInformationListFromContest(contestId, template, questions, attributes, options));
 		}
 
 		return correctnessInformationList;
 
+	}
+
+	/**
+	 * Retrieves the list of {@link CorrectnessInformation} related to the given {@code contestId}, {@code template}, {@code questions}, {@code
+	 * attributes}, and {@code options} of a {@link Contest}, ensuring that the order of the {@link Question}s is the same as they appear on the voter
+	 * portal.
+	 * <p>
+	 * Matching the order of {@link Question}s, two different business logics must be applied:
+	 * <ul>
+	 *   <li>For a contest with template {@value CombinedCorrectnessInformation#OPTIONS_TEMPLATE} (containing referendum-style questions), the order
+	 *   of {@link Question}s is determined by the {@link ElectionAttributes} of the {@link Contest}.</li>
+	 *   <li>For a contest with template {@value CombinedCorrectnessInformation#LISTS_AND_CANDIDATES_TEMPLATE}, the order of {@link Question}s is
+	 *   the same as they appear on the voter portal as long as we ensure that the "lists" question comes before the
+	 *   {@value CombinedCorrectnessInformation#CANDIDATES} question.</li>
+	 * </ul>
+	 *
+	 * @param contestId  the id of the {@link Contest}.
+	 * @param template   the template of the {@link Contest}.
+	 * @param questions  the list of {@link Question}s of the {@link Contest}.
+	 * @param attributes the list of {@link ElectionAttributes} of the {@link Contest}.
+	 * @param options    the list of {@link ElectionOption} of the {@link Contest}.
+	 * @return the correctness information list.
+	 */
+	private static List<CorrectnessInformation> getCorrectnessInformationListFromContest(final String contestId, final String template,
+			final List<Question> questions, final List<ElectionAttributes> attributes, final List<ElectionOption> options) {
+
+		if (OPTIONS_TEMPLATE.equals(template)) {
+			return getCorrectnessInformationListFromOptionsTemplateContest(questions, attributes, options, contestId);
+
+		} else if (LISTS_AND_CANDIDATES_TEMPLATE.equals(template)) {
+			return getCorrectnessInformationListFromListsAndCandidatesTemplate(questions, attributes, options, contestId);
+
+		} else {
+			throw new CombinedCorrectnessInformationException(
+					String.format("Contests with template \"%s\" are not supported. [contestId=%s]", template, contestId));
+		}
+	}
+
+	/**
+	 * Retrieves the list of {@link CorrectnessInformation} related to the given {@code questions}, {@code attributes}, {@code options}, and {@code
+	 * contestId} of a {@link Contest}.
+	 * <p>
+	 * In the case of a contest's template {@value CombinedCorrectnessInformation#OPTIONS_TEMPLATE}, the order of the correctness information list is
+	 * the same as the order in which the attributes appear in the {@link Contest}.
+	 *
+	 * @param questions  the list of {@link Question}s of the {@link Contest}.
+	 * @param attributes the list of {@link ElectionAttributes} of the {@link Contest}.
+	 * @param options    the list of {@link ElectionOption} of the {@link Contest}.
+	 * @param contestId  the id of the contest.
+	 * @return the correctness information list.
+	 */
+	private static List<CorrectnessInformation> getCorrectnessInformationListFromOptionsTemplateContest(final List<Question> questions,
+			final List<ElectionAttributes> attributes, final List<ElectionOption> options, final String contestId) {
+
+		final List<CorrectnessInformation> correctnessInformationList = new ArrayList<>();
+
+		for (final ElectionAttributes electionAttributes : attributes) {
+			if (!electionAttributes.isCorrectness()) {
+				continue;
+			}
+
+			final String correctnessId = electionAttributes.getId();
+			final Integer numberOfVotingOptions = getNumberOfVotingOptions(attributes, options, correctnessId);
+			final Question question = getCorrespondingQuestionByAttribute(questions, correctnessId, contestId);
+			final Integer maximalSelections = question.getMax();
+
+			correctnessInformationList.add(new CorrectnessInformation(correctnessId, maximalSelections, numberOfVotingOptions));
+		}
+
+		return correctnessInformationList;
+	}
+
+	/**
+	 * Retrieves the list of {@link CorrectnessInformation} related to the given {@code questions}, {@code attributes}, and {@code options} of a
+	 * {@link Contest}.
+	 * <p>
+	 * In the case of a contest's template {@value CombinedCorrectnessInformation#LISTS_AND_CANDIDATES_TEMPLATE}, the order of the correctness
+	 * information list must ensure that the "lists" question comes before the {@value CombinedCorrectnessInformation#CANDIDATES} question.
+	 *
+	 * @param questions  the list of {@link Question}s of the {@link Contest}.
+	 * @param attributes the list of {@link ElectionAttributes} of the {@link Contest}.
+	 * @param options    the list of {@link ElectionOption} of the {@link Contest}.
+	 * @param contestId  the id of the contest.
+	 * @return the correctness information list.
+	 */
+	private static List<CorrectnessInformation> getCorrectnessInformationListFromListsAndCandidatesTemplate(final List<Question> questions,
+			final List<ElectionAttributes> attributes, final List<ElectionOption> options, final String contestId) {
+
+		checkQuestionsSizeOfListsAndCandidatesContest(contestId, questions);
+
+		if (isSwappingOfQuestionsNeeded(questions, attributes)) {
+			Collections.swap(questions, 0, 1);
+		}
+
+		final List<CorrectnessInformation> correctnessInformationList = new ArrayList<>();
+
+		for (final Question question : questions) {
+
+			final String correctnessId = question.getAttribute();
+			final Integer numberOfVotingOptions = getNumberOfVotingOptions(attributes, options, correctnessId);
+			final Integer maximalSelections = question.getMax();
+
+			correctnessInformationList.add(new CorrectnessInformation(correctnessId, maximalSelections, numberOfVotingOptions));
+		}
+
+		return correctnessInformationList;
 	}
 
 	private void initCombinedCorrectnessInformation() {
@@ -177,26 +273,29 @@ public class CombinedCorrectnessInformation implements HashableList {
 	}
 
 	/**
-	 * Computes the number of voting options for the given {@code correctnessId} within the given {@code contest}.
+	 * Computes the number of voting options for the given {@code correctnessId} within the given {@code attributes} and {@code electionOptions} of a
+	 * contest.
 	 * <p>
 	 * For example, a correctnessId might identify a specific question. This question has three associated voting options (called election options in
 	 * the Ballot model): YES, NO, EMPTY. Therefore, the method should return 3 in this case. Another example would be a correctnessId identifying the
 	 * selection of a list in an election. Imagine that the voter can choose between 20 different lists. In this case, the method should return 21 (20
 	 * lists + the blank list).
 	 *
-	 * @param contest       the contest.
-	 * @param correctnessId the correctness id.
+	 * @param attributes      the list of {@link ElectionAttributes} of the {@link Contest}.
+	 * @param electionOptions the list {@link ElectionOption} of the {@link Contest}.
+	 * @param correctnessId   the correctness id.
 	 * @return the computed number of voting options.
 	 */
-	private static Integer getNumberOfVotingOptions(final Contest contest, final String correctnessId) {
+	private static Integer getNumberOfVotingOptions(final List<ElectionAttributes> attributes, final List<ElectionOption> electionOptions,
+			final String correctnessId) {
 
 		// list of the related election attributes ids, ie list of election attributes ids whose field related (an array) contains the value correctnessId.
-		final List<String> relatedElectionAttributesIdsList = contest.getAttributes().stream()
+		final List<String> relatedElectionAttributesIdsList = attributes.stream()
 				.filter(electionAttributes -> electionAttributes.getRelated() != null && electionAttributes.getRelated().contains(correctnessId))
 				.map(ElectionAttributes::getId).collect(Collectors.toList());
 
 		// the number of voting options is the number of election options which are present in the list of the related election attributes ids.
-		final long numberOfVotingOptions = contest.getOptions().stream()
+		final long numberOfVotingOptions = electionOptions.stream()
 				.filter(electionOption -> relatedElectionAttributesIdsList.contains(electionOption.getAttribute())).count();
 
 		return Math.toIntExact(numberOfVotingOptions);
@@ -246,6 +345,12 @@ public class CombinedCorrectnessInformation implements HashableList {
 		return correctnessIdToListOfIndexesMap;
 	}
 
+	private static Question getCorrespondingQuestionByAttribute(final List<Question> questions, final String attribute, final String contestId) {
+		return questions.stream().filter(question -> question.getAttribute().equals(attribute)).findAny().orElseThrow(
+				() -> new CombinedCorrectnessInformationException(
+						String.format("No corresponding question found in contest with id %s for attribute with id %s.", contestId, attribute)));
+	}
+
 	private static Integer computeTotalNumberOfSelections(final List<CorrectnessInformation> correctnessInformationList) {
 		return correctnessInformationList.stream().map(CorrectnessInformation::getNumberOfSelections).reduce(0, Integer::sum);
 	}
@@ -255,21 +360,22 @@ public class CombinedCorrectnessInformation implements HashableList {
 	}
 
 	/**
-	 * The {@link Ballot} object orders the "{@link Question}s" (which can correspond to a selection of referendum-type questions, but also to a
-	 * selection of a list or a number of candidates) according to the way the "questions" appear on the voter portal. However, in case of an election
-	 * with lists and candidates (template {@value CombinedCorrectnessInformation#LISTS_AND_CANDIDATES_TEMPLATE} and 2 questions), if the first
-	 * question corresponds to an election attribute with the alias {@value CombinedCorrectnessInformation#CANDIDATES}, we need to swap it with the
-	 * second question. This swap ensures the first question relates to "lists" and the second to {@value CombinedCorrectnessInformation#CANDIDATES}.
+	 * Indicates if a swapping of question is needed.
+	 * <p>
+	 * The {@link Ballot} object does not order the "{@link Question}s" (which can correspond to a selection of referendum-type questions, but also to
+	 * a selection of a list or a number of candidates) according to the way the "questions" appear on the voter portal. In case of an election with
+	 * lists and candidates (contest's template {@value CombinedCorrectnessInformation#LISTS_AND_CANDIDATES_TEMPLATE} and {@value
+	 * CombinedCorrectnessInformation#MAX_LISTS_AND_CANDIDATES_QUESTIONS_SIZE} questions), if the first question corresponds to an election attribute
+	 * with the alias {@value CombinedCorrectnessInformation#CANDIDATES}, we need to swap it with the second question. This swap ensures the first
+	 * question relates to "lists" and the second to {@value CombinedCorrectnessInformation#CANDIDATES}.
 	 *
-	 * @param template   the template of the {@link Contest}.
 	 * @param questions  the list of {@link Question}s of the {@link Contest}.
 	 * @param attributes the list of {@link ElectionAttributes} of the {@link Contest}.
 	 * @return true if the swapping is needed according to the conditions, false otherwise.
 	 */
-	private static boolean isSwappingOfQuestionsNeeded(final String template, final List<Question> questions,
-			final List<ElectionAttributes> attributes) {
+	private static boolean isSwappingOfQuestionsNeeded(final List<Question> questions, final List<ElectionAttributes> attributes) {
 
-		if (LISTS_AND_CANDIDATES_TEMPLATE.equals(template) && questions.size() == 2) {
+		if (questions.size() == MAX_LISTS_AND_CANDIDATES_QUESTIONS_SIZE) {
 			final Question firstQuestion = questions.get(0);
 
 			return attributes.stream().filter(ElectionAttributes::isCorrectness)
@@ -280,20 +386,22 @@ public class CombinedCorrectnessInformation implements HashableList {
 		return false;
 	}
 
-	private static void checkQuestionsSizeOfContest(final String contestId, final String template, final List<Question> questions) {
-		if (LISTS_AND_CANDIDATES_TEMPLATE.equals(template) && questions.size() > MAX_LISTS_AND_CANDIDATES_QUESTIONS_SIZE) {
+	private static void checkQuestionsSizeOfListsAndCandidatesContest(final String contestId, final List<Question> questions) {
+		if (questions.size() > MAX_LISTS_AND_CANDIDATES_QUESTIONS_SIZE) {
 			throw new CombinedCorrectnessInformationException(
 					String.format("A contest with template \"%s\" cannot have more than %s questions. [contestId=%s, questions size of contest=%s]",
 							LISTS_AND_CANDIDATES_TEMPLATE, MAX_LISTS_AND_CANDIDATES_QUESTIONS_SIZE, contestId, questions.size()));
 		}
 	}
 
-	private static void checkContestsNotNullAndNotEmpty(final List<Contest> contests, final String ballotId) {
+	private static List<Contest> checkContestsNotNullAndNotEmpty(final List<Contest> contests, final String ballotId) {
 		if (contests == null) {
 			throw new CombinedCorrectnessInformationException(String.format("The provided contests for the ballot with id %s are null.", ballotId));
 		} else if (contests.isEmpty()) {
 			throw new CombinedCorrectnessInformationException(String.format("The provided contests for the ballot with id %s are empty.", ballotId));
 		}
+
+		return contests;
 	}
 
 	private static void checkNotNullAndNotEmpty(final List<?> parameterList, final String parameterListContentDescription, final String contestId) {
