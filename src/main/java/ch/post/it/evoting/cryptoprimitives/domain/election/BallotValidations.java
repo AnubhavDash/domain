c248971f5b035e4cd7fbdfe446eb5f34ee4bcf5a
jenkinsevoting <spoc-dev-evoting@post.ch>
2022-06-03 10:09:24 +0200

    merge release crypto-primitives-domain 0.14.2.2

--- src/main/java/ch/post/it/evoting/cryptoprimitives/domain/election/BallotValidations.java
+++ src/main/java/ch/post/it/evoting/cryptoprimitives/domain/election/BallotValidations.java
@@ -26,16 +26,16 @@ public final class BallotValidations {
 	public static void checkQuestionsSizeOfListsAndCandidatesContest(final String contestId, final List<Question> questions) {
 		if (questions.size() > Contest.MAX_LISTS_AND_CANDIDATES_QUESTIONS_SIZE) {
 			throw new IllegalArgumentException(
-					String.format("A contest with template \"%s\" cannot have more than %s questions. [contestId=%s, questions size of contest=%s]",
+					String.format("A contest with template \"%s\" cannot have more than %s questions. [contestId: %s, questions size of contest: %s]",
 							Contest.LISTS_AND_CANDIDATES_TEMPLATE, Contest.MAX_LISTS_AND_CANDIDATES_QUESTIONS_SIZE, contestId, questions.size()));
 		}
 	}
 
 	public static List<Contest> checkContestsNotNullAndNotEmpty(final List<Contest> contests, final String ballotId) {
 		if (contests == null) {
-			throw new IllegalArgumentException(String.format("The ballot contains a null contests list. [ballotId=%s]", ballotId));
+			throw new IllegalArgumentException(String.format("The ballot contains a null contests list. [ballotId: %s]", ballotId));
 		} else if (contests.isEmpty()) {
-			throw new IllegalArgumentException(String.format("The ballot contains an empty contests list. [ballotId=%s]", ballotId));
+			throw new IllegalArgumentException(String.format("The ballot contains an empty contests list. [ballotId: %s]", ballotId));
 		}
 
 		return contests;
@@ -44,10 +44,10 @@ public final class BallotValidations {
 	public static void checkNotNullAndNotEmpty(final List<?> parameterList, final String parameterListContentDescription, final String contestId) {
 		if (parameterList == null) {
 			throw new IllegalArgumentException(
-					String.format("The contest contains a null %s list. [contestId=%s]", parameterListContentDescription, contestId));
+					String.format("The contest contains a null %s list. [contestId: %s]", parameterListContentDescription, contestId));
 		} else if (parameterList.isEmpty()) {
 			throw new IllegalArgumentException(
-					String.format("The contest contains an empty %s list. [contestId=%s]", parameterListContentDescription, contestId));
+					String.format("The contest contains an empty %s list. [contestId: %s]", parameterListContentDescription, contestId));
 		}
 	}
 }
